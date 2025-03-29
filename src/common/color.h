bbd777fe202f10ee8152064310d5294806215fa2
elegracer <elegracer@gmail.com>
2025-03-30 00:12:23 +0800

    feat: display normal as color map

--- src/common/color.h
+++ src/common/color.h
@@ -12,18 +12,25 @@ inline Vec3i scale_normalized_color(const Vec3d& color_normalized, const int sca
         .matrix();
 }
 
-inline bool hit_sphere(const Vec3d& center, const double radius, const Ray& ray) {
+inline double hit_sphere(const Vec3d& center, const double radius, const Ray& ray) {
     const Vec3d oc = center - ray.origin();
     const double a = ray.direction().squaredNorm();
     const double b = -2.0 * ray.direction().dot(oc);
     const double c = oc.squaredNorm() - radius * radius;
     const double discriminant = b * b - 4.0 * a * c;
-    return discriminant >= 0.0;
+
+    if (discriminant < 0.0) {
+        return -1.0;
+    }
+
+    return (-b - std::sqrt(discriminant)) / (2.0 * a);
 }
 
 inline Vec3d ray_color(const Ray& ray) {
-    if (hit_sphere(Vec3d {0.0, 0.0, -1.0}, 0.5, ray)) {
-        return Vec3d {1.0, 0.0, 0.0};
+    const double t = hit_sphere(Vec3d {0.0, 0.0, -1.0}, 0.5, ray);
+    if (t > 0.0) {
+        const Vec3d normal = (ray.at(t) - Vec3d {0.0, 0.0, -1.0}).normalized();
+        return 0.5 * (normal + Vec3d::Ones());
     }
 
     const Vec3d unit_direction = ray.direction().normalized();
