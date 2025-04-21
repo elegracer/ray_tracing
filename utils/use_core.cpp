6384a829878e7b39b872ab8bc7ac5aa03200ff49
elegracer <elegracer@gmail.com>
2025-04-22 00:56:23 +0800

    feat: new scene - checkered spheres

--- utils/use_core.cpp
+++ utils/use_core.cpp
@@ -1,7 +1,8 @@
 #include "common/sphere.h"
-#include "core/core.h"
+#include "core/version.h"
 
 #include <fmt/core.h>
+#include <fmt/ostream.h>
 #include <Eigen/Eigen>
 #include <argparse/argparse.hpp>
 #include <opencv2/opencv.hpp>
@@ -12,37 +13,15 @@
 #include "common/material.h"
 #include "common/bvh.h"
 
-int main(int argc, const char* argv[]) {
 
-    std::string output_image_format {};
-    std::string scene_to_render {};
-
-    argparse::ArgumentParser program("use_core");
-    program.add_description(core::get_project_description());
-
-    program.add_argument("--output_image_format")
-        .help("Output Image Format, e.g. jpg, png, bmp etc.")
-        .default_value("png")
-        .store_into(output_image_format);
-
-    program.add_argument("--scene")
-        .help("Scene to render")
-        .choices("bouncing_spheres", "checkered_spheres")
-        .default_value("bouncing_spheres")
-        .store_into(scene_to_render);
-
-    program.parse_args(argc, argv);
-
-    fmt::print("output_image_format: {}\n", output_image_format);
-    fmt::print("scene to render: {}\n", scene_to_render);
+void render_bouncing_spheres(const std::string& output_image_format) {
 
     // World
     HittableList world;
 
-    auto checker =
-        std::make_shared<CheckerTexture>(0.32, Vec3d {0.2, 0.3, 0.1}, Vec3d {0.9, 0.9, 0.9});
+    auto ground_material = std::make_shared<Lambertion>(Vec3d {0.5, 0.5, 0.5});
     world.add(pro::make_proxy_shared<Hittable, Sphere>(Vec3d {0.0, -1000.0, 0.0}, 1000.0,
-        pro::make_proxy_shared<Material, Lambertion>(checker)));
+        ground_material));
 
     for (int a = -11; a < 11; ++a) {
         for (int b = -11; b < 11; ++b) {
@@ -108,6 +87,88 @@ int main(int argc, const char* argv[]) {
     // cv::imshow("output image", cam.img);
     // cv::waitKey();
     cv::imwrite(fmt::format("output.{}", output_image_format), cam.img);
+}
 
-    return 0;
+void render_checkered_spheres(const std::string& output_image_format) {
+
+    // World
+    HittableList world;
+
+    auto checker =
+        std::make_shared<CheckerTexture>(0.32, Vec3d {0.2, 0.3, 0.1}, Vec3d {0.9, 0.9, 0.9});
+
+    world.add(pro::make_proxy_shared<Hittable, Sphere>(Vec3d {0.0, -10.0, 0.0}, 10.0,
+        pro::make_proxy_shared<Material, Lambertion>(checker)));
+    world.add(pro::make_proxy_shared<Hittable, Sphere>(Vec3d {0.0, 10.0, 0.0}, 10.0,
+        pro::make_proxy_shared<Material, Lambertion>(checker)));
+
+    auto bvhnode = pro::make_proxy_shared<Hittable, BVHNode>(world);
+
+    pro::proxy<Hittable> world_as_hittable = bvhnode;
+
+    // Camera
+    Camera cam;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 1280;
+    cam.samples_per_pixel = 500;
+    cam.max_depth = 50;
+
+    cam.vfov = 20.0;
+    cam.lookfrom = {13.0, 2.0, 3.0};
+    cam.lookat = {0.0, 0.0, 0.0};
+    cam.vup = {0.0, 1.0, 0.0};
+
+    cam.defocus_angle = 0.0;
+
+    // Render
+    cam.render(world_as_hittable);
+
+    // cv::imshow("output image", cam.img);
+    // cv::waitKey();
+    cv::imwrite(fmt::format("output.{}", output_image_format), cam.img);
+}
+
+
+int main(int argc, const char* argv[]) {
+
+    const std::string version_string = fmt::format("{}.{}.{}.{}", CORE_MAJOR_VERSION,
+        CORE_MINOR_VERSION, CORE_PATCH_VERSION, CORE_TWEAK_VERSION);
+
+    std::string output_image_format {};
+    std::string scene_to_render {};
+
+    argparse::ArgumentParser program("use_core", version_string);
+
+    program.add_argument("--output_image_format")
+        .help("Output Image Format, e.g. jpg, png, bmp etc.")
+        .default_value("png")
+        .store_into(output_image_format);
+
+    program.add_argument("--scene")
+        .help("Scene to render")
+        .choices("bouncing_spheres", "checkered_spheres")
+        .default_value("checkered_spheres")
+        .store_into(scene_to_render);
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::exception& err) {
+        fmt::print(stderr, "{}\n\n", err.what());
+        fmt::print(stderr, "{}\n", fmt::streamed(program));
+        return EXIT_FAILURE;
+    }
+
+    fmt::print("output_image_format: {}\n", output_image_format);
+    fmt::print("scene to render: {}\n", scene_to_render);
+
+    if (scene_to_render == "bouncing_spheres") {
+        render_bouncing_spheres(output_image_format);
+    } else if (scene_to_render == "checkered_spheres") {
+        render_checkered_spheres(output_image_format);
+    } else {
+        fmt::print(stderr, "Invalid scene to render: '{}' !!!\n", scene_to_render);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
