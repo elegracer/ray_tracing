f10046ed5c10c93febc4d31287eb86b2eb012285
elegracer <elegracer@gmail.com>
2025-04-26 11:09:40 +0800

    feat: smooth perlin noise texture with trilinear interpolation

--- src/common/perlin.h
+++ src/common/perlin.h
@@ -15,10 +15,25 @@ struct Perlin {
     }
 
     double noise(const Vec3d& p) const {
-        const int i = int(4 * p.x()) & 255;
-        const int j = int(4 * p.y()) & 255;
-        const int k = int(4 * p.z()) & 255;
-        return m_rand_float[m_perm_x[i] ^ m_perm_y[j] ^ m_perm_z[k]];
+        const double u = p.x() - std::floor(p.x());
+        const double v = p.y() - std::floor(p.y());
+        const double w = p.z() - std::floor(p.z());
+
+        const int i = int(std::floor(p.x()));
+        const int j = int(std::floor(p.y()));
+        const int k = int(std::floor(p.z()));
+
+        double c[2][2][2];
+        for (int di = 0; di < 2; ++di) {
+            for (int dj = 0; dj < 2; ++dj) {
+                for (int dk = 0; dk < 2; ++dk) {
+                    c[di][dj][dk] = m_rand_float[m_perm_x[(i + di) & 255] ^ m_perm_y[(j + dj) & 255]
+                                                 ^ m_perm_z[(k + dk) & 255]];
+                }
+            }
+        }
+
+        return trilinear_interp(c, u, v, w);
     }
 
 private:
@@ -35,5 +50,19 @@ private:
             const int target_idx = random_int(0, i);
             std::swap(p[i], p[target_idx]);
         }
+    }
+
+    static double trilinear_interp(const double c[2][2][2], const double u, const double v,
+        const double w) {
+        double accum = 0.0;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                for (int k = 0; k < 2; ++k) {
+                    accum += (i * u + (1 - i) * (1.0 - u)) * (j * v + (1 - j) * (1.0 - v))
+                             * (k * w + (1 - k) * (1.0 - w)) * c[i][j][k];
+                }
+            }
+        }
+        return accum;
     }
 };
