18d3d818b98f3f230e8ff2b41abb372a28ac7708
elegracer <elegracer@gmail.com>
2025-04-27 19:02:24 +0800

    feat: estimate pi endlessly to show convergence

--- utils/use_pi.cpp
+++ utils/use_pi.cpp
@@ -6,17 +6,22 @@
 int main(int argc, const char* argv[]) {
 
     constexpr int sample_count = 1000000;
-    int inside_circle_count = 0;
 
-    for (int i = 0; i < sample_count; ++i) {
+    int inside_circle_count = 0;
+    int runs = 0;
+
+    while (true) {
+        ++runs;
         const double x = random_double(-1.0, 1.0);
         const double y = random_double(-1.0, 1.0);
         if (x * x + y * y < 1.0) {
             ++inside_circle_count;
         }
-    }
 
-    fmt::print("Estimate of Pi: {:.12f}\n", (4.0 * inside_circle_count) / sample_count);
+        if (runs % sample_count == 0) {
+            fmt::print("Estimate of Pi: {:.12f}\n", 4.0 * inside_circle_count / runs);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
