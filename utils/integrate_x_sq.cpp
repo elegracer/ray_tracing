9a584b42787deb2793d33ad4e71e03a3297eacfa
elegracer <elegracer@gmail.com>
2025-04-29 22:51:26 +0800

    feat: integrate_x_sq with uniform pdf

--- utils/integrate_x_sq.cpp
+++ utils/integrate_x_sq.cpp
@@ -3,21 +3,27 @@
 
 #include "common/common.h"
 
+inline double icd(const double d) {
+    return 2.0 * d;
+}
+
+inline double pdf(const double x) {
+    return 0.5;
+}
+
 int main(int argc, const char* argv[]) {
 
     constexpr int sample_count_sqrt = 1000;
     constexpr int sample_count = sample_count_sqrt * sample_count_sqrt;
 
-    int a = 0;
-    int b = 2;
     double sum = 0.0;
 
     for (int i = 0; i < sample_count; ++i) {
-        const double x = random_double(a, b);
-        sum += x * x;
+        const double x = icd(random_double());
+        sum += x * x / pdf(x);
     }
 
-    fmt::print("I = {:.12f}\n", (b - a) * (sum / sample_count));
+    fmt::print("I = {:.12f}\n", sum / sample_count);
 
     return EXIT_SUCCESS;
 }
